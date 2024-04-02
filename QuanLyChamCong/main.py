b40eebbb4811b4c3f1abdf026d089970f73c3e3c
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2024-04-02 21:18:08 +0700

    Update main.py: Cập nhật hàm giaoDienDangKy

--- QuanLyChamCong/main.py
+++ QuanLyChamCong/main.py
@@ -171,7 +171,7 @@ def main_window():
         right_frame.config(bg="red")
         right_frame.grid_propagate(False)
         
-    def giaoDienDangKy():
+        def giaoDienDangKy():
         # thông tin nhân viên + chụp hình + lưu
         for widget in right_frame.winfo_children():
             widget.destroy()
@@ -181,6 +181,7 @@ def main_window():
         left_frame_dk = Frame(right_frame, bg="pink",width=600,height=800)
         right_frame_dk = Frame(right_frame, bg="orange",width=600,height=800)
         left_frame_dk.grid(row=0,column=0,sticky="nsew")
+
         right_frame_dk.grid(row=0,column=1,sticky="nsew")
         right_frame.grid_columnconfigure(0, weight=1)
         right_frame.grid_columnconfigure(1, weight=1)
@@ -197,7 +198,7 @@ def main_window():
                 img3=ImageTk.PhotoImage(Image.fromarray(img1))
                 if  len(face_recognition.face_encodings(img1)) >0:
                     dem+=1
-                    cv2.imwrite(f"QuanLyChamCong/imgCheck/{nhapTen.get()}{dem}.png",frame)
+                    cv2.imwrite(f"C:/Users/ACER/Dropbox/My PC (LAPTOP-UGP9QJUT)/Documents/ITstudies/QuanLyChamCong/imgCheck/{nhapTen.get()}{dem}.png",frame)
                     print("được")
                 label_hinhAnh.configure(image=img3,width=360,height=360)
                 right_frame_dk.update() 
@@ -207,17 +208,60 @@ def main_window():
             cap.release()
             cv2.destroyAllWindows()
     
-        # bên trái, bên phải
-            
+        # nhập tên, chụp ảnh
         label_Khung=LabelFrame(right_frame_dk,bg="white",borderwidth=5)
-        label_Khung.pack(pady=50)
-        label_hinhAnh=Label(label_Khung, bg="light gray",text="",width=50,height=25)
+        label_Khung.pack(pady=29)
+        label_hinhAnh=Label(label_Khung, bg="light gray",text="",width=50,height=30)
         label_hinhAnh.pack()
-        nhapTen=Entry(right_frame_dk,width=20)
+        nhapTen=Entry(right_frame_dk,width=20, font=("arial", 15), borderwidth=3)
         nhapTen.pack(pady=20)
-        btn_dangky=CTkButton(right_frame_dk,text="Chụp Ảnh",command=showHinhAnh,width=100,height=100)
+        btn_dangky=CTkButton(right_frame_dk,text="Chụp Ảnh",command=showHinhAnh,width=100,height=50)
         btn_dangky.pack()
 
+        # left_frame_dk chứa các thành phần để nhập thông tin nhân viên
+        label_ten = Label(left_frame_dk, text="Họ tên", width=19, height=3, font=("Arial", 15))
+        label_ten.grid(row=0, column=0, pady=8)
+        text_ten = Entry(left_frame_dk, font=("Arial", 15), borderwidth=3)
+        text_ten.grid(row=0, column=1)
+        label_ngaysinh = Label(left_frame_dk, text="Ngày sinh: ", width=19, height=3, font=("Arial", 15))
+        label_ngaysinh.grid(row=1,column=0, pady=8)
+        text_ngaysinh = Entry(left_frame_dk, font=("Arial", 15), borderwidth=3)
+        text_ngaysinh.grid(row=1,column=1)
+        label_sdt = Label(left_frame_dk, text="Số điện thoại: ", width=19, height=3, font=("Arial", 15))
+        label_sdt.grid(row=2, column=0, pady=8)
+        text_sdt = Entry(left_frame_dk, font=("Arial", 15), borderwidth=3)
+        text_sdt.grid(row=2, column=1)
+        label_gioitinh = Label(left_frame_dk, text="Giới tính: ", width=19, height=3, font=("Arial", 15))
+        label_gioitinh.grid(row=3,column=0, pady=8)
+        value_gioitinh = ["Nam", "Nữ"]
+        combobox_gioitinh = ttk.Combobox(left_frame_dk, values=value_gioitinh, font=("Arial", 15), state="readonly")
+        combobox_gioitinh["width"] = 19
+        combobox_gioitinh.grid(row=3, column=1)
+        label_chucvu = Label(left_frame_dk, text="Chức vụ: ", width=19, height=3, font=("Arial", 15))
+        label_chucvu.grid(row=4,column=0, pady=8)
+        value_chucvu = ["Quản lý", "Nhân viên", "Thực tập"]
+        combobox_chucvu = ttk.Combobox(left_frame_dk, values=value_chucvu, font=("Arial", 15), state="readonly")
+        combobox_chucvu["width"] = 19
+        combobox_chucvu.grid(row=4, column=1)
+        label_email = Label(left_frame_dk, text="Email: ", width=19, height=3, font=("Arial", 15))
+        label_email.grid(row=5, column=0, pady=8)
+        text_email = Entry(left_frame_dk, font=("Arial", 15), borderwidth=3)
+        text_email.grid(row=5, column=1)
+
+
+        # hàm clear các textfield, combobox
+        def button_clear():
+            text_ten.delete(0, END)
+            text_ngaysinh.delete(0, END)
+            text_sdt.delete(0, END)
+            text_email.delete(0, END)
+            combobox_chucvu.set('')
+            combobox_gioitinh.set('')
+
+        button_lammoi = CTkButton(left_frame_dk, text="Làm mới", width=100, height=50, command=button_clear)
+        button_lammoi.grid(row=6, column=1, pady=52)
+        button_xacnhan = CTkButton(left_frame_dk, text="Xác nhận", width=100, height=50)
+        button_xacnhan.grid(row=6, column=2, pady=52)
 
     def giaoDienQuanly():
         right_frame.grid_propagate(False)
